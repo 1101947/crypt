ea68a985a9f36ab5891695866b978f30345e5674
hsfhrwe4r <hsfhrwe4r>
2026-07-11 18:58:13 +0300

    tests passing. Fixed failing test for chacha20poly1305: in cryptochunk directory. It  seems that problem was with wrong aead overhead(32 instead of 16)

--- cryptochunk/cryptochunk_test.go
+++ cryptochunk/cryptochunk_test.go
@@ -2,7 +2,7 @@ package cryptochunk
 
 import (
 	"testing"
-	"fmt"
+	//"fmt"
 	"crypto/rand"
 	"crypt/aes256gcm"
 	"crypt/chacha20poly1305"
@@ -45,48 +45,45 @@ func TestCryptoChunkAes256Gcm(t *testing.T) {
 }
 
 
-
 func TestCryptoChunkChaCha20Poly1305(t *testing.T) {
-	plainData1 := "llllllllllllllllllllll"
-	var plainData2 string
-	chachaOverhead := 32 
-	//plainData2buf := make([]byte, len(plainData1))  
-	cryptData := make([]byte, len(plainData1)+chachaOverhead)
-
-	plainData2buf := make([]byte, len(plainData1))  
-	secretKey := []byte("11111111111111111111111111111111")
-	//nonceSrc := []byte("123456789012")
-	nonceSrc := make([]byte, 24)
-	i, err := rand.Read(nonceSrc)
+	key := make([]byte, 32)
+	i, err := rand.Read(key)
+	if i != 32 {
+		t.Errorf("i must be equal to 32, but it is %d", i)
+	}
+	if err != nil {
+		t.Errorf("ERROR: %v : %d", err, i)
+	}
+	nonce := make([]byte, 24)
+	i, err = rand.Read(nonce)
 	if err != nil {
 		t.Errorf("ERROR %v : %d", err, i)
 	}
 
+	plainData := []byte("la la la just data it is")
+	cipherData := make([]byte, len(plainData)+16)
+
 	chnk1 := CryptChunk{
-		In: []byte(plainData1),
-		Out: cryptData, 
-		Key: secretKey,
-		NonceSource: nonceSrc,
+		In: plainData,
+		Out: cipherData,
+		Key: key,
+		NonceSource: nonce,
 		ChunkPosition: 1,
 		Crypter: chacha20poly1305.GetChaCha20Poly1305(),
 	}
-	//fmt.Printf("chnk1: %+v", chnk1)
 	err = chnk1.Encrypt()
 	if err != nil {
-		t.Errorf("%v", err)
+		t.Errorf("ERROR %v", err)
 	}
-	fmt.Printf("chnk1: %+v\n", chnk1)
+	resultPlainData := make([]byte, len(plainData))
 	chnk1.In = chnk1.Out
-	chnk1.Out = plainData2buf 
-	fmt.Printf("chnk1: %+v\n", chnk1)
+	chnk1.Out = resultPlainData
 	err = chnk1.Decrypt()
-	fmt.Printf("chnk1: %+v\n", chnk1)
 	if err != nil {
-		t.Errorf("%v", err)
+		t.Errorf("ERROR: %v", err)
 	}
-	//fmt.Printf("\nResult: %+v , %+v\n", chnk1.Out, plainData2)
-	plainData2 = string(plainData2buf)
-	if string(plainData1) != plainData2 {
-		t.Errorf("Expected plainData1: \"%s\" to be equal to plainData2: \"%s\", but they are not.", string(plainData1), plainData2)
+	if string(plainData) != string(resultPlainData) {
+		t.Errorf("Expected plainData \"%s\" to be equal to resultPlainData \"%s\"", plainData, resultPlainData)
 	}
 }
+
