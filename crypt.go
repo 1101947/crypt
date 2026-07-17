8f9fbceb7edeb44340f53ed74d7f36d44f4a38b1
hsfhrwe4r <hsfhrwe4r>
2026-07-17 14:15:44 +0300

    Cleaned cryp.go up from comments. The problem that was solved in previous commit was in wrong handling of for loop boundaries(needed to use chunksPos<=chunksAmount in for loop, but i think i used chunksPos<chunksAmount), file.Read insted of io.ReadFull(file, buff)(because file.Read returns 0, io.EOF when have encountered EOF while reading from file to buffer, so no way to know length of the last chunk, which is not the case for io.ReadFull). Also add handling last chunk in both encrypt and decrypt after the for loop. But thats a description what i did in previous commit.

--- crypt.go
+++ crypt.go
@@ -4,38 +4,19 @@ import (
 	"io"
 	"os"
 	"fmt"
-//	//"bufio"
-//	//"strings"
-////	"encoding/json"
 	"crypto/rand"
-////	"crypt/encrypted"
 	"crypt/argon2id"
 	"crypt/aes256gcm"
 	"crypt/header"
 	"crypt/cryptochunk"
-//
 	"golang.org/x/term"
-//	//"github.com/1101947/cliargumentrouter/cmdrouter"
 	"github.com/1101947/cliargumentrouter/flag"
 )
-//
+
 type CryptHandler struct {
 	cryptData cryptData
 	interactive string
 }
-//
-
-// Encrypt
-// Get default params
-// Parse user set params
-// Header.Write
-// Salt.Generate
-// Salt.Write
-// GetKey
-// Nonce.Generate
-// Nonce.Write
-//
-
 
 type EncryptHandler CryptHandler
 
@@ -84,7 +65,6 @@ func (E EncryptHandler) Process(posargs []string) error {
 		return fmt.Errorf("Trying to open file, got : %w", err)
 	}
 	defer inputRD.Close()
-	//E.cryptData.in = inputRD
 
 	outputVals, ok := kwargs["output"]
 	if !ok {
@@ -103,15 +83,7 @@ func (E EncryptHandler) Process(posargs []string) error {
 		return fmt.Errorf("Trying to create file, got : %w", err)
 	}
 	defer outputWR.Close()
-	//E.cryptData.output = outputWR 
 
-	// TODO: uncomment later
-	//err = E.cryptData.Encrypt()
-	//return err
-	// TODO: change this, add flag
-	// consider using newDefaultCryptoData
-	//headr := header.GetDefaultHeader()
-	//crpt := NewCryptData()
 	argonHeader := argon2id.GetDefaultHeader()
 	salt, err := argon2id.GetSalt(argonHeader.SaltLength)
 	if err != nil {
@@ -122,7 +94,6 @@ func (E EncryptHandler) Process(posargs []string) error {
 		Salt: salt,
 	}
 	// Do get key realy need argonParams as a param ?
-	fmt.Printf("DEBUG: %+v\n", argonParams)
 	key, err  := GetKey(argonParams)
 	if err != nil {
 		return fmt.Errorf("Geting key from user, got: %w", err)
@@ -170,10 +141,8 @@ func (E EncryptHandler) Process(posargs []string) error {
 	}
 	err = c.Encrypt()
 	return err 
-
-	//err := c.Decrypt()
 }
-//
+
 func (D DecryptHandler) Process(posargs []string) error {
 	// TODO: maybe put flags inside EncryptionHandler ?
 	flags := flag.DefaultFlags("--", "=", posargs)
@@ -200,7 +169,6 @@ func (D DecryptHandler) Process(posargs []string) error {
 		return fmt.Errorf("Trying to open file, got : %w", err)
 	}
 	defer inputRD.Close()
-	//D.cryptData.input = inputRD
 
 	outputVals, ok := kwargs["output"]
 	if !ok {
@@ -219,40 +187,12 @@ func (D DecryptHandler) Process(posargs []string) error {
 		return fmt.Errorf("Trying to create file, got : %w", err)
 	}
 	defer outputWR.Close()
-	//D.cryptData.output = outputWR 
-
 
 	D.cryptData.in = inputRD
 	D.cryptData.out = outputWR
 	err = D.cryptData.Decrypt()
 	return err 
-	//header := format.GetNewHeader()
-	//header.Decrypt(input, output, key)
-	//fmt.Println("Decrypting...")
 }
-//
-//
-//type cryptData struct {
-//	//sourcePath string
-//	//destPath string
-//	input io.Reader
-//	output io.Writer
-//	symmCryptFuncToUse string
-//	slen uint32
-//	iter uint32
-//	mem uint32
-//	klen uint32
-//	paral uint8
-//}
-//
-//input: nil,
-//output: nil,
-//symmCryptFuncToUse: "aes256gcm", 
-//slen: 16, 
-//iter: 1, 
-//mem: 64*1024,
-//klen: 32, 
-//paral: 4, 
 
 func NewCryptData() cryptData {
 	c := cryptData{
@@ -273,9 +213,7 @@ type cryptData struct {
 
 func (c cryptData) Encrypt() error {
 	var headerBuf [128]byte 
-	// c.h is header.FileHeader
 	c.h.Encode(&headerBuf)
-	// Writing header
 	headerBytesWriten, err := c.out.Write(headerBuf[:])
 	if err != nil {
 		return fmt.Errorf("Trying to write header buffer to file, got: %w", err)
@@ -283,7 +221,6 @@ func (c cryptData) Encrypt() error {
 	if headerBytesWriten != len(headerBuf) {
 		return fmt.Errorf("Number of bytes writen differs from the amount of bytes in headerBuf(128)")
 	}
-	// Writing salt 
 	saltBytesWriten, err := c.out.Write(c.salt)
 	if err != nil {
 		return fmt.Errorf("Trying to write salt buffer to file, got: %w", err)
@@ -291,7 +228,6 @@ func (c cryptData) Encrypt() error {
 	if saltBytesWriten != len(c.salt) {
 		return fmt.Errorf("Number of bytes writen: %d differs from the amount of bytes in c.salt: %d", saltBytesWriten, len(c.salt))
 	}
-	// Writing nonce 
 	nonceBytesWriten, err := c.out.Write(c.cr.NonceSource)
 	if err != nil {
 		return fmt.Errorf("Trying to write nonce source buffer to file, got: %w", err)
@@ -300,21 +236,16 @@ func (c cryptData) Encrypt() error {
 		return fmt.Errorf("Number of bytes writen: %d differs from the amount of bytes in c.cr.NonceSource: %d", nonceBytesWriten, len(c.cr.NonceSource))
 	}
 
-	// encbody
-
 	// cryptBuf and plainBuf are just cr.Out and cr.In
 	// TODO:
 	// their making should be done in handler, not here
-	fmt.Printf("DEBUG: %+v\n", c.cr)
 	var chunksAmount uint16
 	chunksAmount = uint16(0)
 	var lastChunkSize uint16
 	var readIntoPlain int
 	var writeToOut int
 
-	fmt.Printf("DEBUG: chunks amount: %d \n", chunksAmount)
 	for {
-		//readIntoPlain, err = c.in.Read(c.cr.In)
 		readIntoPlain, err = io.ReadFull(c.in, c.cr.In)
 		if err == io.ErrUnexpectedEOF {
 			break
@@ -326,12 +257,9 @@ func (c cryptData) Encrypt() error {
 			return fmt.Errorf("Trying to read bytes from file into buffer, got: %w", err)
 		}
 		if readIntoPlain <= 0 {
-		//if readIntoPlain < 0 {
 			return fmt.Errorf("Have read invalid number of bytes: %d", readIntoPlain)
 		} 
-		// cr is cryptochunk
 		chunksAmount++
-		fmt.Printf("DEBUG: chunk: %d , len: %d, cap: %d", chunksAmount, len(c.cr.In), cap(c.cr.In))
 		c.cr.ChunkPosition = chunksAmount 
 		err = c.cr.Encrypt()
 		if err != nil {
@@ -344,19 +272,14 @@ func (c cryptData) Encrypt() error {
 		if writeToOut != len(c.cr.Out) {
 			return fmt.Errorf("Writing wrong number of bytes to output file. Should be equal to size of output buffer, but differs.")
 		}
-		//c.cr.In = c.cr.In[:0]
-		fmt.Printf(" after len: %d, cap: %d\n", len(c.cr.In), cap(c.cr.In))
 	}
 	if readIntoPlain < 0 {
 		return fmt.Errorf("Invalid number of bytes read from file: negative: %d", readIntoPlain)
 	}
 	lastChunkSize = 0
-	fmt.Println("DEBUG: readIntoPlain : %d\n", readIntoPlain)
 	if readIntoPlain > 0 {
-		//c.cr.In[readIntoPlain:]
 		lastChunkSize = uint16(readIntoPlain) + c.h.Overhead 
 		c.cr.ChunkPosition = chunksAmount + 1
-		fmt.Printf("DEBUG: last chunk! %+v\n", c.cr)
 		err = c.cr.Encrypt()
 		if err != nil {
 			return fmt.Errorf("Encrypting, got: %w", err)
@@ -387,7 +310,6 @@ func (c cryptData) Encrypt() error {
 	if headerBytesWriten != len(headerBuf) {
 		return fmt.Errorf("Number of bytes writen differs from the amount of bytes in headerBuf(128)")
 	}
-	fmt.Printf("DEBUG: %+v\n", c.h)
 	return nil
 }
 
@@ -401,8 +323,6 @@ func (c cryptData) Decrypt() error {
 		return fmt.Errorf("Read wrong number of bytes. Must have been read %d bytes, but actualy read %d .", len(headerBuf), readIntoHeaderBuf)
 	}
 	c.h.Decode(&headerBuf)
-	fmt.Printf("DEBUG: %+v\n", c.h)
-	// mv
 	saltBuff := make([]byte, int(c.h.ArgonParams.SaltLength)) 
 	readIntoSaltBuff, err := c.in.Read(saltBuff)
 	if err != nil {
@@ -416,8 +336,6 @@ func (c cryptData) Decrypt() error {
 		Header: c.h.ArgonParams,
 		Salt: saltBuff,
 	}
-	//
-	fmt.Printf("DEBUG: %+v\n", argonParams)
 	key, err  := GetKey(argonParams)
 	if err != nil {
 		return fmt.Errorf("Geting key from user, got: %w", err)
@@ -438,7 +356,6 @@ func (c cryptData) Decrypt() error {
 	if err != nil {
 		return fmt.Errorf("Getting overhead, got: %w", err)
 	}
-//	headr := header.GetDefaultHeader()
 	plainDataChunkSize := c.h.ChunkSize - overhead 
 	plainBuf := make([]byte, plainDataChunkSize)
 
@@ -458,26 +375,20 @@ func (c cryptData) Decrypt() error {
 		out: c.out,
 	}
 
-//	readNonceSource, err := c.in.Read(c.cr.NonceSource)
-//	if err != nil {
-//		return fmt.Errorf("Reading nonce source bytes, got: %w", err)
-//	}
 //	// What about comparison with c.h.NonceSourceLen ?
 //	if readNonceSource != len(c.cr.NonceSource) {
 //		return fmt.Errorf("Number of nonce source bytes read from file: %d differ from length of nonce source buffer: %d", readNonceSource, len(c.cr.NonceSource))
 //	}
-	//fmt.Printf("DEBUG: %+v\n", c.cr)
 	var readIntoCrypt int
 	var readIntoPlain int
 	var writeToOut int
 	var chunksPos int
 	chunksAmount := int(c.h.ChunksAmount)
-	fmt.Printf("DEBUG: chunks amount: %d %d\n", c.h.ChunksAmount, chunksAmount)
 	for chunksPos=1;chunksPos<=chunksAmount;chunksPos++ {
-		fmt.Printf("DEBUG: chunk: %d\n", chunksPos)
 		readIntoCrypt, err = c.in.Read(c.cr.In)
+		// TODO: maybe remove ?
 		if err == io.EOF && readIntoPlain == 0 {
-			//TODO: handle
+
 		}
 		if err != nil {
 			return fmt.Errorf("Trying to read bytes from file into buffer, got: %w", err)
@@ -499,7 +410,6 @@ func (c cryptData) Decrypt() error {
 			return fmt.Errorf("Writing wrong number of bytes to output file. Should be equal to size of output buffer, but differs.")
 		}
 	}
-	fmt.Println("Ahhhhhtung!")
 	if c.h.LastChunkSize != 0 {
 		readIntoCrypt, err = c.in.Read(c.cr.In)
 		if err != nil && err != io.EOF {
@@ -509,10 +419,8 @@ func (c cryptData) Decrypt() error {
 			return fmt.Errorf("Have read invalid number of bytes")
 		} 
 		c.cr.ChunkPosition = c.cr.ChunkPosition + 1
-		fmt.Printf("DEBUG: last chunk! %+v\n", c.cr)
 		err = c.cr.Decrypt()
 		if err != nil {
-			fmt.Println("Arrrh, the problem here!")
 			return fmt.Errorf("Decrypting, got: %w", err)
 		}
 		realData := (c.h.LastChunkSize - c.h.Overhead)
@@ -523,53 +431,10 @@ func (c cryptData) Decrypt() error {
 		if writeToOut != len(c.cr.Out[:realData]) {
 			return fmt.Errorf("Writing wrong number of bytes to output file. Should be equal to size of output buffer, but differs.")
 		}
-		fmt.Println("DEBUG: write to out: %d\n", writeToOut)
-
-
 	}
 	return nil
 }
 
-
-//
-//func (c cryptData) Encrypt() error {
-//	if c.input == nil {
-//		return fmt.Errorf("Input is not set")
-//	}
-//	if c.output == nil {
-//		return fmt.Errorf("Input is not set")
-//	}
-////	data, err := io.ReadAll(c.input)
-////	if err != nil {
-////		return fmt.Errorf("Trying to read file, got: %w", err)
-////	}
-//
-////	salt, err := GenSalt(int(c.slen))
-////	if err != nil {
-////		return err
-////	}
-//	key, err  := GetKey()
-//	if err != nil {
-//		return fmt.Errorf("Geting key from user, got: %w", err)
-//	}
-//
-//	header := format.Header{}
-//	err := header.Encrypt(input, output, key)
-//	if err != nil {
-//		return err
-//	}
-//	return nil
-//}
-//
-//func GenSalt(length int) ([]byte, error) {
-//	salt := make([]byte, length)
-//	_, err := rand.Read(salt)
-//	if err != nil {
-//		return nil, err
-//	}
-//	return salt, nil
-//}
-//
 func GetKey(P argon2id.Params) ([]byte, error) {
 	fmt.Println("Provide password: ")
 	s, err := term.ReadPassword(1)
